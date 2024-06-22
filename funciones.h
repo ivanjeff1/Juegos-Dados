a667078b463ecc2f3c9500b9144a018036398643
Candela Mathieu <candemathieu12@gmail.com>
2024-06-22 19:25:33 -0300

    Agrego modificaciones

--- funciones.h
+++ funciones.h
@@ -2,8 +2,8 @@
 
 void menu();
 int jugarRonda();
-int jugarModoUnJugador(char nombre[]);
-void jugarModoDosJugadores(char nombre1[], char nombre2[]);
+int jugarModoUnJugador(char nombre[], int puntajeMaxAnterior, char nombreMaxAnterior[]);
+void jugarModoDosJugadores(char nombre1[], char nombre2[], int puntajeMaxAnterior, char nombreMaxAnterior[]);
 void mostrarPuntajeMaximo(int puntajeMax, char nombreMax[]);
 void copiarVector(char v[], char v2[], int tam);
 
